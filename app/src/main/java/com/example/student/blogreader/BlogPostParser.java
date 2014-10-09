5713733c52ddfbf57898c84e500fa5ad3884eb0f
Bradey Foster <ballislife671@gmail.com>
2014-10-09 14:31:25 -0700

    Accessed a key-value pair from a JSON object

--- app/src/main/java/com/example/student/blogreader/BlogPostParser.java
+++ app/src/main/java/com/example/student/blogreader/BlogPostParser.java
@@ -2,6 +2,7 @@ package com.example.student.blogreader;
 
 import android.util.Log;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
@@ -51,6 +52,11 @@ public class BlogPostParser {
     }
 
     public void readFeed(JSONObject jsonObject){
-        
+        try {
+            JSONArray jsonPosts = jsonObject.getJSONArray("posts");
+        }
+        catch(JSONException error){
+            Log.e("BlogPostParser", "JSONException: " + error);
+        }
     }
 }
