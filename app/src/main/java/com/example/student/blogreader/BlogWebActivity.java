733f1eea136918d5f800ec0b448fbc31845e41f5
Bradey Foster <ballislife671@gmail.com>
2014-10-30 13:48:26 -0800

    WebView ProgressBar

--- app/src/main/java/com/example/student/blogreader/BlogWebActivity.java
+++ app/src/main/java/com/example/student/blogreader/BlogWebActivity.java
@@ -4,19 +4,33 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class BlogWebActivity extends Activity{
+    protected ProgressBar progressBar;
+    protected WebView webView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog_web);
+        progressBar = (ProgressBar)findViewById(R.id.progressBarWebView);
+        webView = (WebView)findViewById(R.id.webView);
 
         Intent intent = getIntent();
         Uri blogUri = intent.getData();
 
-        WebView webView = (WebView)findViewById(R.id.webView);
         webView.loadUrl(blogUri.toString());
-        //webview is being displayed
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                progressBar.setVisibility(View.GONE);
+            }
+
+        });
     }
 }
